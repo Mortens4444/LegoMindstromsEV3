42a31ad20cb5e0247710c4d30e3102d3d2be103d
Mortens <mortens.4444@gmail.com>
2022-03-20 14:32:13 +0100

    Fix flags behavior of OutputPort

--- Mindstorms.Core/Enums/OutputPort.cs
+++ Mindstorms.Core/Enums/OutputPort.cs
@@ -9,6 +9,21 @@ namespace Mindstorms.Core.Enums
 		public static readonly OutputPort C = new OutputPort(0x04, nameof(C));
 		public static readonly OutputPort D = new OutputPort(0x08, nameof(D));
 
+		public static readonly OutputPort AB = new OutputPort(A | B, nameof(AB));
+		public static readonly OutputPort AC = new OutputPort(A | C, nameof(AC));
+		public static readonly OutputPort AD = new OutputPort(A | D, nameof(AD));
+		public static readonly OutputPort BC = new OutputPort(B | C, nameof(BC));
+		public static readonly OutputPort BD = new OutputPort(B | D, nameof(BD));
+		public static readonly OutputPort CD = new OutputPort(C | D, nameof(CD));
+
+		public static readonly OutputPort ABC = new OutputPort(A | B | C, nameof(ABC));
+		public static readonly OutputPort ABD = new OutputPort(A | B | D, nameof(ABD));
+		public static readonly OutputPort ACD = new OutputPort(A | C | D, nameof(ACD));
+		public static readonly OutputPort BCD = new OutputPort(B | C | D, nameof(BCD));
+
+		public static readonly OutputPort ABCD = new OutputPort(A | B | C | D, nameof(ABCD));
+
+
 		private OutputPort(byte value, string name) : base(value, name)
 		{
 			Values.Add(value, this);
